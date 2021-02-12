34cfc812c7bec1dafaa9a34628b0cb24e26fe242
azharfatrr <m.azhar.faturahman@gmail.com>
2021-02-13 00:23:09 +0700

    test

--- kernel.c
+++ kernel.c
@@ -1,40 +1,59 @@
-static CurLine = 0xa0*0x10;
 
-int main () {
-  printHelloWorld();
-  printHelloWorld();
-  while (1);
-}
-int get_curr_line(){
-    CurLine =  CurLine + 0xa0;
-    return CurLine;
-}
-printHelloWorld(){
-  int line = get_curr_line();
-  putInMemory(0xB000, 0x8000 + line, 'H');
-  putInMemory(0xB000, 0x8001+ line, 0xD);
-  putInMemory(0xB000, 0x8002+ line, 'e');
-  putInMemory(0xB000, 0x8003+ line, 0xD);
-  putInMemory(0xB000, 0x8004+ line, 'l');
-  putInMemory(0xB000, 0x8005+ line, 0xD);
-  putInMemory(0xB000, 0x8006+ line, 'l');
-  putInMemory(0xB000, 0x8007+ line, 0xD);
-  putInMemory(0xB000, 0x8008+ line, 'o');
-  putInMemory(0xB000, 0x8009+ line, 0xD);
-  putInMemory(0xB000, 0x800a+ line, ' ');
-  putInMemory(0xB000, 0x800b+ line, 0xD);
-  putInMemory(0xB000, 0x800c+ line, 'w');
-  putInMemory(0xB000, 0x800d+ line, 0xD);
-  putInMemory(0xB000, 0x800e + line, 'o');
-  putInMemory(0xB000, 0x800f+ line, 0xD);
-  putInMemory(0xB000, 0x8010+ line, 'r');
-  putInMemory(0xB000, 0x8011+ line, 0xD);
-  putInMemory(0xB000, 0x8012+ line, 'l');
-  putInMemory(0xB000, 0x8013+ line, 0xD);
-  putInMemory(0xB000, 0x8014+ line, 'd');
-  putInMemory(0xB000, 0x8015+ line, 0xD);
+// KAMUS
+// #define VIDEO_SEGMENT 0xB000
+// #define VIDEO_BASE 0x8000
+#define INT_10H 0x10
+
+
+void clearScreen();
+void tampil();
+void handleInterrupt21 (int AX, int BX, int CX, int DX);
+void interrupt(int number, int AX, int BX, int CX, int DX);
+// // void printString(char *string);
+// // void readString(char *string);
+// void clear(char *buffer, int length); //Fungsi untuk mengisi buffer dengan 0
+
+int main() {
+      
+    clearScreen();
+    tampil();
+    
+    // makeInterrupt21();
+    while (1);
 }
 
+void clearScreen() {
+    interrupt(0x10,0x0000,0,0,0);
+
+}
+
+void tampil() {
+    int ASCI_BASE = 0x0E00;
+    interrupt(INT_10H, ASCI_BASE+'\n', 0, 0, 0);
+    interrupt(INT_10H, ASCI_BASE+'H', 0, 0, 0);
+    interrupt(INT_10H, ASCI_BASE+'e', 0, 0, 0);
+    interrupt(INT_10H, ASCI_BASE+'l', 0, 0, 0);
+    interrupt(INT_10H, ASCI_BASE+'l', 0, 0, 0);
+    interrupt(INT_10H, ASCI_BASE+'o', 0, 0, 0);
+    interrupt(INT_10H, ASCI_BASE+' ', 0, 0, 0);
+    interrupt(INT_10H, ASCI_BASE+'W', 0, 0, 0);
+    interrupt(INT_10H, ASCI_BASE+'o', 0, 0, 0);
+    interrupt(INT_10H, ASCI_BASE+'r', 0, 0, 0);
+    interrupt(INT_10H, ASCI_BASE+'l', 0, 0, 0);
+    interrupt(INT_10H, ASCI_BASE+'d', 0, 0, 0);
+    interrupt(INT_10H, ASCI_BASE+'!', 0, 0, 0);
+}
 
 
-void handleInterrupt21 (int AX, int BX, int CX, int DX) {}
+void handleInterrupt21 (int AX, int BX, int CX, int DX){
+    // switch (AX) {
+    //     case 0x0:
+    //         // printString(BX);
+    //         break;
+    //     case 0x1:
+    //         // readString(BX);
+    //         break;
+    //     default:
+    //         // printString("Invalid interrupt");
+    // }
+}
