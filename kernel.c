4ef1177eb4647f427452db4123df1d810aa26ab5
azharfatrr <m.azhar.faturahman@gmail.com>
2021-02-12 22:06:24 +0700

    Add script

--- kernel.c
+++ kernel.c
@@ -1,34 +1,10 @@
 int main () {
-  putInMemory(0xB000, 0x8000, 'H');
-  putInMemory(0xB000, 0x8001, 0xD);
-  putInMemory(0xB000, 0x8002, 'a');
-  putInMemory(0xB000, 0x8003, 0xD);
-  putInMemory(0xB000, 0x8004, 'i');
-  putInMemory(0xB000, 0x8005, 0xD);
-
-  while (1);
+    putInMemory(0xB000, 0x80A0, 'H');
+    putInMemory(0xB000, 0x80A1, 0xD);
+    putInMemory(0xB000, 0x80A2, 'a');
+    putInMemory(0xB000, 0x80A3, 0xD);
+    putInMemory(0xB000, 0x80A4, 'i');
+    putInMemory(0xB000, 0x80A5, 0xD);
+    while (1);
 }
-
-/* Ini deklarasi fungsi */
-void handleInterrupt21 (int AX, int BX, int CX, int DX);
-void printString(char *string);
-void readString(char *string);
-void clear(char *buffer, int length); //Fungsi untuk mengisi buffer dengan 0
-
-int main() {
-  makeInterrupt21();
-  while (1);
-}
-
-void handleInterrupt21 (int AX, int BX, int CX, int DX){
-  switch (AX) {
-    case 0x0:
-      printString(BX);
-      break;
-    case 0x1:
-      readString(BX);
-      break;
-    default:
-      printString("Invalid interrupt");
-  }
-}
+void handleInterrupt21 (int AX, int BX, int CX, int DX) {}
