e3aa90e3441161c8857f107d15e8bf3780afe518
rayfazt <rayfazt@gmail.com>
2021-02-12 21:59:00 +0700

    nambah template fungsi

--- kernel.c
+++ kernel.c
@@ -9,4 +9,26 @@ int main () {
   while (1);
 }
 
-void handleInterrupt21 (int AX, int BX, int CX, int DX) {}
+/* Ini deklarasi fungsi */
+void handleInterrupt21 (int AX, int BX, int CX, int DX);
+void printString(char *string);
+void readString(char *string);
+void clear(char *buffer, int length); //Fungsi untuk mengisi buffer dengan 0
+
+int main() {
+  makeInterrupt21();
+  while (1);
+}
+
+void handleInterrupt21 (int AX, int BX, int CX, int DX){
+  switch (AX) {
+    case 0x0:
+      printString(BX);
+      break;
+    case 0x1:
+      readString(BX);
+      break;
+    default:
+      printString("Invalid interrupt");
+  }
+}
