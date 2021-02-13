923b127d0406ff181b071c195c0c5663704d9ac4
azharfatrr <m.azhar.faturahman@gmail.com>
2021-02-13 09:35:20 +0700

    add printString

--- kernel.c
+++ kernel.c
@@ -5,55 +5,63 @@
 #define INT_10H 0x10
 
 
-void clearScreen();
+void clearScreen(int mode);
 void tampil();
 void handleInterrupt21 (int AX, int BX, int CX, int DX);
-void interrupt(int number, int AX, int BX, int CX, int DX);
-// // void printString(char *string);
+int interrupt(int number, int AX, int BX, int CX, int DX);
+void printString(char *string);
 // // void readString(char *string);
 // void clear(char *buffer, int length); //Fungsi untuk mengisi buffer dengan 0
 
 int main() {
       
-    clearScreen();
-    tampil();
+    clearScreen(0);
+    printString("Hello World\n");
+    // tampil();
     
     // makeInterrupt21();
     while (1);
 }
 
-void clearScreen() {
-    interrupt(0x10,0x0000,0,0,0);
-
+void clearScreen(int mode) {
+    /* mode 0 : default text
+       mode 1 : text ART
+       mode 2 : graphical
+    */
+    switch (mode)
+    {
+    case 1:
+        interrupt(0x10,0x0000,0,0,0);
+        break;
+    case 2:
+        interrupt(0x10,0x0013,0,0,0);
+        break;
+    default:
+        interrupt(0x10,0x0003,0,0,0);
+        break;
+    }
 }
 
-void tampil() {
-    int ASCI_BASE = 0x0E00;
-    interrupt(INT_10H, ASCI_BASE+'\n', 0, 0, 0);
-    interrupt(INT_10H, ASCI_BASE+'H', 0, 0, 0);
-    interrupt(INT_10H, ASCI_BASE+'e', 0, 0, 0);
-    interrupt(INT_10H, ASCI_BASE+'l', 0, 0, 0);
-    interrupt(INT_10H, ASCI_BASE+'l', 0, 0, 0);
-    interrupt(INT_10H, ASCI_BASE+'o', 0, 0, 0);
-    interrupt(INT_10H, ASCI_BASE+' ', 0, 0, 0);
-    interrupt(INT_10H, ASCI_BASE+'W', 0, 0, 0);
-    interrupt(INT_10H, ASCI_BASE+'o', 0, 0, 0);
-    interrupt(INT_10H, ASCI_BASE+'r', 0, 0, 0);
-    interrupt(INT_10H, ASCI_BASE+'l', 0, 0, 0);
-    interrupt(INT_10H, ASCI_BASE+'d', 0, 0, 0);
-    interrupt(INT_10H, ASCI_BASE+'!', 0, 0, 0);
+void printString(char *string) {
+    int ASCII = 0x0E00;
+    int i = 0;
+    while (string[i] != '\0') {
+        interrupt(INT_10H, ASCII + string[i],0,0,0);
+        i++;
+    }
 }
+
 
 
 void handleInterrupt21 (int AX, int BX, int CX, int DX){
-    // switch (AX) {
-    //     case 0x0:
-    //         // printString(BX);
-    //         break;
-    //     case 0x1:
-    //         // readString(BX);
-    //         break;
-    //     default:
-    //         // printString("Invalid interrupt");
-    // }
+    switch (AX) {
+        case 0x0:
+            printString(BX);
+            break;
+        case 0x1:
+            // readString(BX);
+            break;
+        default:
+            printString("Invalid interrupt");
+    }
 }
