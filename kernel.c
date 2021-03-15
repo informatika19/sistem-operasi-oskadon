e45a7e3e1fa2a4d23389ff40e491bc3121bcc0f4
ronggurmahendra <ronggur.mwp@gmail.com>
2021-03-15 16:45:16 +0700

    fixing some problem within shell

--- kernel.c
+++ kernel.c
@@ -56,6 +56,16 @@ int modeScreen(int mode);
 void drawSquare();
 void drawSomething();
 void asciiART();
+//buat debugging
+
+void shell();
+void executecmd(char* cmd);
+int modifiedstrcmp(char* a, char* b, int len,int start);
+int ignoreSpace(char* cmd, int start);
+void cd(char* param);
+void ls(char* param);
+void cat(char* param);
+void ln(char* param);
 
 
 int main() {
@@ -65,7 +75,6 @@ int main() {
     char b2[512];
     int* sectors;
 
-    
     // Tampilkan tampilan awal bios dengan graphic
     dump = modeScreen(2);    // Ganti mode menjadi graph mode (Sekalian clear screen)
     drawSomething();
@@ -89,9 +98,8 @@ int main() {
     b1[3] = 'd';
     // writeFile(b1,"/sys/cek3",sectors,0xFF);
     // readFile(b2,"/sys/cek3",sectors,0xFF);
-
+    shell();
     // printString(b2);
-
 
     while (1) {
         // Loop selamanya untuk meminta input string dari user dan menampilkannya pada layar
@@ -757,3 +765,90 @@ bool isFirstLetter(char* first, char* compare) {
 
     return true;
 }
+
+
+
+
+//testing
+/*
+void shell(){
+    char cmd[180];
+    //char cmd1[180]; //history
+    //char cmd2[180]; //history
+    //char cmd3[180]; //history
+    //int HistHEAD = 1;
+    printString("Executing Shell...\n");
+    while(1){
+        printString("$ ");
+        readString(cmd);
+        //printString(cmd);
+        executecmd(cmd);
+        //strcpy(cmd2,cmd3);
+        //strcpy(cmd1,cmd2);
+        //strcpy(cmd,cmd1);
+        //clear(cmd,180);
+    }
+
+}
+
+void executecmd(char* cmd){
+    int cmdIndex = 0;
+    if(modifiedstrcmp(cmd,"cd",2,cmdIndex)){
+        cmdIndex += 2;
+        cmdIndex = ignoreSpace(cmd,cmdIndex);
+        cd(&cmd[cmdIndex]);
+    }else if(modifiedstrcmp(cmd,"ls",2,cmdIndex)){
+        cmdIndex += 2;
+        cmdIndex = ignoreSpace(cmd,cmdIndex);
+        ls(&cmd[cmdIndex]);
+    }else if(modifiedstrcmp(cmd,"cat",3,cmdIndex)){
+        cmdIndex += 3;
+        cmdIndex = ignoreSpace(cmd,cmdIndex);
+        cat(&cmd[cmdIndex]);
+    }else{
+        cmdIndex = ignoreSpace(cmd,cmdIndex);
+        printString("invalid command\n");
+    }
+
+}
+
+int modifiedstrcmp(char* a, char* b, int len,int start){ //compare untuk beberapa char pertama aja dimulai dari index sesuati *buat shell
+    int i;
+    i = 0;
+    while(i < len){
+        if(a[start + i] != b[i]){
+            return 0;
+        }
+        i++;
+    }
+    return 1;
+}
+
+int ignoreSpace(char* cmd, int start){ //return new index
+    while(cmd[start] == 0x20){
+        start++;
+    }
+    return start;
+}
+
+void cd(char* param){
+    printString("calling cd with parar : ");
+    printString(param);
+    printString("\n");
+}
+void ls(char* param){
+    printString("calling ls with parar : ");
+    printString(param);
+    printString("\n");
+}
+void cat(char* param){
+    printString("calling cat with parar : ");
+    printString(param);
+    printString("\n");
+}
+void ln(char* param){
+    printString("calling ln with parar : ");
+    printString(param);
+    printString("\n");
+}
+*/
--- shell.c
+++ shell.c
@@ -3,51 +3,51 @@ void shell();
 void executecmd(char* cmd);
 int modifiedstrcmp(char* a, char* b, int len,int start);
 int ignoreSpace(char* cmd, int start);
+void cd(char* param);
+void ls(char* param);
+void cat(char* param);
+void ln(char* param);
 
 
-void main(){
+void shell(){
     char cmd[180];
-    char cmd1[180]; //history
-    char cmd2[180]; //history
-    char cmd3[180]; //history
+    //char cmd1[180]; //history
+    //char cmd2[180]; //history
+    //char cmd3[180]; //history
     //int HistHEAD = 1;
     printString("Executing Shell...\n");
-    printString("$ ");
     while(1){
+        printString("$ ");
         readString(cmd);
         //printString(cmd);
         executecmd(cmd);
-        strcpy(cmd2,cmd3);
-        strcpy(cmd1,cmd2);
-        strcpy(cmd,cmd1);
-        clear(cmd,180);
+        //strcpy(cmd2,cmd3);
+        //strcpy(cmd1,cmd2);
+        //strcpy(cmd,cmd1);
+        //clear(cmd,180);
     }
 
 }
 
 void executecmd(char* cmd){
     int cmdIndex = 0;
-    while(1){// implementasi jangan lupa ilangin loop dan breaknya
-        if(modifiedstrcmp(cmd,"cd",2,cmdIndex)){
-            cmdIndex += 2;
-            cmdIndex = ignoreSpace(cmd,cmdIndex);
-            printString("calling cd\n");
-        }else if(modifiedstrcmp(cmd,"ls",2,cmdIndex)){
-            cmdIndex += 2;
-            cmdIndex = ignoreSpace(cmd,cmdIndex);
-            printString("calling ls\n");
-        }else if(modifiedstrcmp(cmd,"cat",3,cmdIndex)){
-            cmdIndex += 3;
-            cmdIndex = ignoreSpace(cmd,cmdIndex);
-            printString("calling cat\n");
-        }else if(cmd[cmdIndex] == 0x0 || cmd[cmdIndex] == "\n"){
-            cmdIndex = ignoreSpace(cmd,cmdIndex);
-            printString("end of command\n");
-        }else{
-            cmdIndex = ignoreSpace(cmd,cmdIndex);
-            printString("invalid command\n");
-        }
+    if(modifiedstrcmp(cmd,"cd",2,cmdIndex)){
+        cmdIndex += 2;
+        cmdIndex = ignoreSpace(cmd,cmdIndex);
+        cd(&cmd[cmdIndex]);
+    }else if(modifiedstrcmp(cmd,"ls",2,cmdIndex)){
+        cmdIndex += 2;
+        cmdIndex = ignoreSpace(cmd,cmdIndex);
+        ls(&cmd[cmdIndex]);
+    }else if(modifiedstrcmp(cmd,"cat",3,cmdIndex)){
+        cmdIndex += 3;
+        cmdIndex = ignoreSpace(cmd,cmdIndex);
+        cat(&cmd[cmdIndex]);
+    }else{
+        cmdIndex = ignoreSpace(cmd,cmdIndex);
+        printString("invalid command\n");
     }
+
 }
 
 int modifiedstrcmp(char* a, char* b, int len,int start){ //compare untuk beberapa char pertama aja dimulai dari index sesuati *buat shell
@@ -67,4 +67,25 @@ int ignoreSpace(char* cmd, int start){ //return new index
         start++;
     }
     return start;
+}
+
+void cd(char* param){
+    printString("calling cd with parar : ");
+    printString(param);
+    printString("\n");
+}
+void ls(char* param){
+    printString("calling ls with parar : ");
+    printString(param);
+    printString("\n");
+}
+void cat(char* param){
+    printString("calling cat with parar : ");
+    printString(param);
+    printString("\n");
+}
+void ln(char* param){
+    printString("calling ln with parar : ");
+    printString(param);
+    printString("\n");
 }
