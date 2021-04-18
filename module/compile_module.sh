f420ab3da8b9a501e28a748ca41ad68d96222ff5
azharfatrr <m.azhar.faturahman@gmail.com>
2021-04-18 20:27:57 +0700

    edit compile

--- module/compile_module.sh
+++ module/compile_module.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-bcc -ansi -c -o ../obj/fileIO.o fileIO.c
-bcc -ansi -c -o ../obj/folderIO.o folderIO.c
-bcc -ansi -c -o ../obj/math.o math.c
-bcc -ansi -c -o ../obj/sector.o sector.c
-bcc -ansi -c -o ../obj/text.o text.c
+bcc -ansi -c -o ../fileIO.o fileIO.c
+bcc -ansi -c -o ../folderIO.o folderIO.c
+bcc -ansi -c -o ../math.o math.c
+bcc -ansi -c -o ../sector.o sector.c
+bcc -ansi -c -o ../text.o text.c
 echo "Compile Success!"
