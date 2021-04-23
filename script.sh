4f6fa7f3bb3e6f0109f021501291d718455c5368
azharfatrr <m.azhar.faturahman@gmail.com>
2021-04-23 15:52:26 +0700

    Drop files from .gitignore

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@
 dd if=/dev/zero of=system.img bs=512 count=2880
 nasm bootloader.asm -o bootloader
 dd if=bootloader of=system.img bs=512 count=1 conv=notrunc
-dd if=image.img of=system.img bs=512 count=128 seek=1024 conv=notrunc
+dd if=image2binary/image.img of=system.img bs=512 count=128 seek=1024 conv=notrunc
 
 # Input file system
 dd if=map.img of=system.img bs=512 count=1 seek=256 conv=notrunc
