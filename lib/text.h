1c3e0108444a35b2b844af2fb83b93194c78c4ee
rayfazt <rayfazt@gmail.com>
2021-04-23 19:35:04 +0700

    cp done

--- lib/text.h
+++ lib/text.h
@@ -2,6 +2,7 @@
 #define _STRING_H_
 
 #include "math.h"
+#include "boolean.h"
 
 int strlen(char* buff);
 int strcmp(char* a, char* b);
@@ -9,6 +10,12 @@ void strcpy(char* dest, char* src);
 void clear(char* buffer, int length);
 void printString(char* string);
 void readString(char* string);
-
 int ignoreSpace(char* cmd, int start);
+
+/** Fungsi penunjang */
+bool isFlExist(char* dir, int parrentIdx, char* name, bool folder, int* foundIdx);
+void getFlName(char* files,char filesIdx, char* name);
+int foundEmptyDir(char* dir);
+void writeDir(char* dir, int dirNum, int parrentIdx, int sectorIdx, char* name);
+
 #endif
